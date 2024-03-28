9118a04f76bef3d763cb57e3fd0b5dd8b63ee7a5
BERNARDO BONZOUMET JAMES <202102368618@alunos.ibmec.edu.br>
2024-03-27 22:13:26 -0300

    codigo de mensagem mqtt utilizando a esp32

--- mqtt_test/mqtt_esp32_test.ino
+++ mqtt_test/mqtt_esp32_test.ino
@@ -1,61 +1,195 @@
-#include <WiFi.h>
-#include <PubSubClient.h>
-
-// WiFi
-const char *ssid = "Wifizonee"; // Enter your Wi-Fi name
-const char *password = "sistemasembarcados";  // Enter Wi-Fi password
-
-// MQTT Broker
-const char *mqtt_broker = "test.mosquitto.org";
-const char *topic = "emqx/esp32";
-const char *mqtt_username = "emqx";
-const char *mqtt_password = "public";
-const int mqtt_port = 1883;
-
+//Programa: comunicação MQTT com ESP32
+//Autor: Pedro Bertoleti
+ 
+/* Headers */ 
+#include <WiFi.h> /* Header para uso das funcionalidades de wi-fi do ESP32 */
+#include <PubSubClient.h>  /*  Header para uso da biblioteca PubSubClient */
+ 
+/* Defines do MQTT */
+/* IMPORTANTE: recomendamos fortemente alterar os nomes
+               desses tópicos. Caso contrário, há grandes
+               chances de você enviar e receber mensagens de um ESP32
+               de outra pessoa.
+*/
+/* Tópico MQTT para recepção de informações do broker MQTT para ESP32 */
+#define TOPICO_SUBSCRIBE "esp32"   
+/* Tópico MQTT para envio de informações do ESP32 para broker MQTT */
+#define TOPICO_PUBLISH   "esp32"  
+/* id mqtt (para identificação de sessão) */
+/* IMPORTANTE: este deve ser único no broker (ou seja, 
+               se um client MQTT tentar entrar com o mesmo 
+               id de outro já conectado ao broker, o broker 
+               irá fechar a conexão de um deles).
+*/
+#define ID_MQTT  "INCB_Cliente_MQTT"     
+/*  Variáveis e constantes globais */
+/* SSID / nome da rede WI-FI que deseja se conectar */
+const char* SSID = "Wifizonee"; 
+/*  Senha da rede WI-FI que deseja se conectar */
+const char* PASSWORD = "sistemasembarcados"; 
+  
+/* URL do broker MQTT que deseja utilizar */
+const char* BROKER_MQTT = "test.mosquitto.org"; 
+/* Porta do Broker MQTT */
+int BROKER_PORT = 1883;
+ 
+ 
+/* Variáveis e objetos globais */
 WiFiClient espClient;
-PubSubClient client(espClient);
-
-void setup() {
-    // Set software serial baud to 115200;
+PubSubClient MQTT(espClient);
+  
+//Prototypes
+void init_serial(void);
+void init_wifi(void);
+void init_mqtt(void);
+void reconnect_wifi(void); 
+void mqtt_callback(char* topic, byte* payload, unsigned int length);
+void verifica_conexoes_wifi_mqtt(void);
+ 
+/* 
+ *  Implementações das funções
+ */
+void setup() 
+{
+    init_serial();
+    init_wifi();
+    init_mqtt();
+}
+  
+/* Função: inicializa comunicação serial com baudrate 115200 (para fins de monitorar no terminal serial 
+*          o que está acontecendo.
+* Parâmetros: nenhum
+* Retorno: nenhum
+*/
+void init_serial() 
+{
     Serial.begin(115200);
-    // Connecting to a WiFi network
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.println("Connecting to WiFi..");
+}
+ 
+/* Função: inicializa e conecta-se na rede WI-FI desejada
+ * Parâmetros: nenhum
+ * Retorno: nenhum
+ */
+void init_wifi(void) 
+{
+    delay(10);
+    Serial.println("------Conexao WI-FI------");
+    Serial.print("Conectando-se na rede: ");
+    Serial.println(SSID);
+    Serial.println("Aguarde");
+    reconnect_wifi();
+}
+  
+/* Função: inicializa parâmetros de conexão MQTT(endereço do  
+ *         broker, porta e seta função de callback)
+ * Parâmetros: nenhum
+ * Retorno: nenhum
+ */
+void init_mqtt(void) 
+{
+    /* informa a qual broker e porta deve ser conectado */
+    MQTT.setServer(BROKER_MQTT, BROKER_PORT); 
+    /* atribui função de callback (função chamada quando qualquer informação do 
+    tópico subescrito chega) */
+    MQTT.setCallback(mqtt_callback);            
+}
+  
+/* Função: função de callback 
+ *          esta função é chamada toda vez que uma informação de 
+ *          um dos tópicos subescritos chega)
+ * Parâmetros: nenhum
+ * Retorno: nenhum
+ * */
+void mqtt_callback(char* topic, byte* payload, unsigned int length) 
+{
+    String msg;
+ 
+    //obtem a string do payload recebido
+    for(int i = 0; i < length; i++) 
+    {
+       char c = (char)payload[i];
+       msg += c;
     }
-    Serial.println("Connected to the Wi-Fi network");
-    //connecting to a mqtt broker
-    client.setServer(mqtt_broker, mqtt_port);
-    client.setCallback(callback);
-    while (!client.connected()) {
-        String client_id = "esp32-client-";
-        client_id += String(WiFi.macAddress());
-        Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
-        if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
-            Serial.println("Public EMQX MQTT broker connected");
-        } else {
-            Serial.print("failed with state ");
-            Serial.print(client.state());
+    Serial.print("[MQTT] Mensagem recebida: ");
+    Serial.println(msg);     
+}
+  
+/* Função: reconecta-se ao broker MQTT (caso ainda não esteja conectado ou em caso de a conexão cair)
+ *          em caso de sucesso na conexão ou reconexão, o subscribe dos tópicos é refeito.
+ * Parâmetros: nenhum
+ * Retorno: nenhum
+ */
+void reconnect_mqtt(void) 
+{
+    while (!MQTT.connected()) 
+    {
+        Serial.print("* Tentando se conectar ao Broker MQTT: ");
+        Serial.println(BROKER_MQTT);
+        if (MQTT.connect(ID_MQTT)) 
+        {
+            Serial.println("Conectado com sucesso ao broker MQTT!");
+            MQTT.subscribe(TOPICO_SUBSCRIBE); 
+        } 
+        else
+        {
+            Serial.println("Falha ao reconectar no broker.");
+            Serial.println("Havera nova tentatica de conexao em 2s");
             delay(2000);
         }
     }
-    // Publish and subscribe
-    client.publish(topic, "Hi, I'm ESP32 ^^");
-    client.subscribe(topic);
 }
-
-void callback(char *topic, byte *payload, unsigned int length) {
-    Serial.print("Message arrived in topic: ");
-    Serial.println(topic);
-    Serial.print("Message:");
-    for (int i = 0; i < length; i++) {
-        Serial.print((char) payload[i]);
+  
+/* Função: reconecta-se ao WiFi
+ * Parâmetros: nenhum
+ * Retorno: nenhum
+*/
+void reconnect_wifi() 
+{
+    /* se já está conectado a rede WI-FI, nada é feito. 
+       Caso contrário, são efetuadas tentativas de conexão */
+    if (WiFi.status() == WL_CONNECTED)
+        return;
+         
+    WiFi.begin(SSID, PASSWORD);
+     
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(100);
+        Serial.print(".");
     }
+   
     Serial.println();
-    Serial.println("-----------------------");
+    Serial.print("Conectado com sucesso na rede ");
+    Serial.print(SSID);
+    Serial.println("IP obtido: ");
+    Serial.println(WiFi.localIP());
 }
-
-void loop() {
-    client.loop();
+ 
+/* Função: verifica o estado das conexões WiFI e ao broker MQTT. 
+ *         Em caso de desconexão (qualquer uma das duas), a conexão
+ *         é refeita.
+ * Parâmetros: nenhum
+ * Retorno: nenhum
+ */
+void verifica_conexoes_wifi_mqtt(void)
+{
+    /* se não há conexão com o WiFI, a conexão é refeita */
+    reconnect_wifi(); 
+    /* se não há conexão com o Broker, a conexão é refeita */
+    if (!MQTT.connected()) 
+        reconnect_mqtt(); 
+} 
+ 
+/* programa principal */
+void loop() 
+{   
+    /* garante funcionamento das conexões WiFi e ao broker MQTT */
+    verifica_conexoes_wifi_mqtt();
+    /* Envia frase ao broker MQTT */
+    MQTT.publish(TOPICO_PUBLISH, "abc");
+ 
+    /* keep-alive da comunicação com broker MQTT */    
+    MQTT.loop();
+    /* Agurda 1 segundo para próximo envio */
+    delay(1000);   
 }
